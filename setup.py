3ca0497812a51cdb594a3345903cafc5d775fc45
xmq <1776866992@qq.com>
2024-12-12 09:40:07 +0800

    1

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 Created on 2024-11-29
 """
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
-with open('README.md', encoding='utf-8')as fp:
-      long_description = fp.read()
+with open('README.md', encoding='utf-8') as fp:
+    long_description = fp.read()
 
 setup(name='soeasypack',
       description='简易精准打包python项目和依赖环境 \nEasy and precise packaging of python projects and dependencies',
@@ -14,16 +14,16 @@ setup(name='soeasypack',
       author_email='xmqsvip@qq.com',
       long_description=long_description,
       long_description_content_type='text/markdown',
-      packages=find_packages(),
+      packages=find_namespace_packages(),
       package_data={
-      'soeasypack': ['dep_exe/**/*']},
+          'soeasypack': ['dep_exe/**/*']},
       include_package_data=True,
       license='MIT',
-      version='0.7.0',
+      version='0.7.3',
       zip_safe=False,
-      url = "https://github.com/XMQSVIP/SoEasyPack",
+      url="https://github.com/XMQSVIP/SoEasyPack",
       classifiers=[
-            'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3',
       ],
       keywords="soeasypack,SoEasyPack,packaging,pyinstaller,py2exe,cxfreeze",
       install_requires=["Cython"]
