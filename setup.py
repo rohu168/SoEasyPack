c3cb00ce0c639454358339cdce21efacc2058f85
xmq <1776866992@qq.com>
2025-01-05 21:04:45 +0800

    1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(name='soeasypack',
           'soeasypack': ['dep_exe/**/*']},
       include_package_data=True,
       license='MIT',
-      version='0.9.1',
+      version='0.9.2',
       zip_safe=False,
       url="https://github.com/XMQSVIP/SoEasyPack",
       classifiers=[
